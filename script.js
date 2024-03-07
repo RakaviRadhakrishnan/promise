fe684eace9b0130804983ce01f8e9df0928e5dba
Rakavi Radhakrishnan <153973093+RakaviRadhakrishnan@users.noreply.github.com>
2024-03-07 21:04:35 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const root = document.getElementById("root");
+const allItems = fetch("https://restcountries.com/v3.1/all");
+const list = document.createElement("ol");
+allItems
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const newData = data
+      .map((item) => {
+        const li = document.createElement("li");
+        const link = document.createElement("a");
+        link.innerText = item.name.common;
+        link.setAttribute("href", "http://google.com");
+        link.style = "text-decoration:none;color:black";
+        li.append(link);
+        list.append(li);
+      });
+    root.append(list);
+  });
+
+
+  
